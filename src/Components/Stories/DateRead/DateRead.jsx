ac17b8090584e541eacf470babb0abc5ce2cf507
Alex981110 <alex.mtz873@gmail.com>
2022-01-23 23:48:25 -0600

    [Add](Stories) Creación de historias incluyendo la navegación al hacer click en ellas

---
+++ src/Components/Stories/DateRead/DateRead.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Styles from './DateRead.module.css';
+import { Link } from 'react-router-dom';
+
+const DateRead = () => {
+  return (
+    <div className={Styles.padding}>
+      <div className='d-flex'>
+        <Link to={"/author"}>
+          <img
+            src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&d=mm&r=x"
+            alt="Sal"
+            height={40}
+            style={{ borderRadius: 20, marginRight: 10 }}
+          />
+        </Link>
+        <div >
+          <span>
+            <Link className={Styles.noUnderline} to={"/author"}>Steve</Link>
+          </span>
+          <br />
+          <span className={Styles.date}>22 July 2017 - 6 min read
+            <Link to={"/post"} title="Read Story">
+              <svg
+                width={25}
+                height={25}
+                viewBox="0 0 25 25"
+              >
+                <path
+                  d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default DateRead;
--- src/Components/Stories/Stories.jsx
+++ src/Components/Stories/Stories.jsx
@@ -1,14 +1,79 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Styles from './Stories.module.css';
+import demo1 from '../../assets/DemoPic/1.jpg';
+import demo2 from '../../assets/DemoPic/2.jpg';
+import demo3 from '../../assets/DemoPic/3.jpg';
+import demo4 from '../../assets/DemoPic/4.jpg';
+import DateRead from './DateRead/DateRead';
 
 const Stories = () => {
   return (
     <>
       <Navbar />
-      <div>
+      <div className={Styles.mainContainer}>
         <h1>Digital Diary</h1>
-        <p>Medium style, simply perfect for bloggers</p>
+        <p className={Styles.subTitle + ' mb-5'}>Medium style, simply perfect for bloggers</p>
 
+        <span className={Styles.titleLabel}>Featured Stories</span>
+        <hr></hr>
+
+        <div className='d-flex'>
+          <div className="card mb-3 me-3">
+            <div className="row g-0">
+              <div className={Styles.imageContainer + " col-md-4"} style={{ backgroundImage: `url(${demo1})` }}></div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className={Styles.cardTitle}>We're living some strange times</h5>
+                  <p className={Styles.cardDescription}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <DateRead />
+              </div>
+            </div>
+          </div>
+
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className={Styles.imageContainer + " col-md-4"} style={{ backgroundImage: `url(${demo2})` }}></div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className={Styles.cardTitle}>We're living some strange times</h5>
+                  <p className={Styles.cardDescription}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <DateRead />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='d-flex'>
+          <div className="card mb-3 me-3">
+            <div className="row g-0">
+              <div className={Styles.imageContainer + " col-md-4"} style={{ backgroundImage: `url(${demo3})` }}></div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className={Styles.cardTitle}>We're living some strange times</h5>
+                  <p className={Styles.cardDescription}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <DateRead />
+              </div>
+            </div>
+          </div>
+
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className={Styles.imageContainer + " col-md-4"} style={{ backgroundImage: `url(${demo4})` }}></div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className={Styles.cardTitle}>We're living some strange times</h5>
+                  <p className={Styles.cardDescription}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+
+                <DateRead />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
