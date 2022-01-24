089889008315312168b3ebc0a4b7a7678979a173
Alex981110 <alex.mtz873@gmail.com>
2022-01-23 23:46:22 -0600

    [Add](Navbar) Navegación en navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Styles from './Navbar.module.css';
 import icon from '../../assets/logoo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className={"nav " + Styles.mainContainer}>
 
-      <img style={{ height: 30, width: 30 }} src={icon} />
+      <img style={{ height: 30, width: 30, marginLeft: 30}} src={icon} />
 
       <div className={Styles.items}>
-        <span className={Styles.links}>Stories</span>
-        <span className={Styles.links}>Post</span>
-        <span className={Styles.links}>Author</span>
+        <Link className={Styles.noUnderline} to={"/"}><span className={Styles.links}>Stories</span></Link>
+        <Link className={Styles.noUnderline} to={"/post"}><span className={Styles.links}>Post</span></Link>
+        <Link className={Styles.noUnderline} to={"/author"}><span className={Styles.links}>Author</span></Link>
         <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
+          <input className="form-control me-1" type="search" placeholder="Buscar" aria-label="Search" />
           <button className="btn btn-primary" type="text">Buscar</button>
         </form>
       </div>
