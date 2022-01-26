74bfe43609189f6ccad6f669416d8b7a31fb05cc
Alex981110 <alex.mtz873@gmail.com>
2022-01-25 22:16:25 -0600

    [Fix](DateRead) asignacion del nombre  autor como prop

--- src/Components/Stories/DateRead/DateRead.jsx
+++ src/Components/Stories/DateRead/DateRead.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Styles from './DateRead.module.css';
 import { Link } from 'react-router-dom';
 
-const DateRead = () => {
+const DateRead = ({name}) => {
   return (
     <div className={Styles.padding}>
       <div className='d-flex'>
@@ -16,7 +16,7 @@ const DateRead = () => {
         </Link>
         <div >
           <span>
-            <Link className={Styles.noUnderline} to={"/author"}>Steve</Link>
+            <Link className={Styles.noUnderline} to={"/author"}>{name}</Link>
           </span>
           <br />
           <span className={Styles.date}>22 July 2017 - 6 min read
@@ -25,6 +25,7 @@ const DateRead = () => {
                 width={25}
                 height={25}
                 viewBox="0 0 25 25"
+                style={{marginLeft: 20}}
               >
                 <path
                   d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z"
