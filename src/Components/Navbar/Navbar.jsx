4de369431161b58506e855b21925c749cffe4c25
Alex981110 <alex.mtz873@gmail.com>
2022-01-29 20:28:03 -0600

    [Fix](Navbar) Responsividad

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,17 +6,16 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className={"nav " + Styles.mainContainer}>
-
-      <img style={{ height: 30, width: 30, marginLeft: 30}} src={icon} />
-
-      <div className={Styles.items}>
-        <Link className={Styles.noUnderline} to={"/"}><span className={Styles.links}>Stories</span></Link>
-        <Link className={Styles.noUnderline} to={"/post"}><span className={Styles.links}>Post</span></Link>
-        <Link className={Styles.noUnderline} to={"/author"}><span className={Styles.links}>Author</span></Link>
-        <form className="d-flex">
-          <input className="form-control me-1" type="search" placeholder="Buscar" aria-label="Search" />
-          <button className="btn btn-primary" type="text">Buscar</button>
-        </form>
+      <div className={Styles.items + " container-lg"}>
+        <img style={{ height: 30, width: 30 }} src={icon} />
+        <div className='d-flex'>
+          <Link className={Styles.noUnderline} to={"/"}><span className={Styles.links}>Stories</span></Link>
+          <Link className={Styles.noUnderline} to={"/post"}><span className={Styles.links}>Post</span></Link>
+          <Link className={Styles.noUnderline} to={"/author"}><span className={Styles.links}>Author</span></Link>
+          <form className="d-flex">
+            <input className={Styles.search + " form-control me-1"} type="search" placeholder="Search" aria-label="Search" />
+          </form>
+        </div>
       </div>
     </div>
   )
