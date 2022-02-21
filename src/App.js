9f1a2a09ea7f12922bd30bca07b5a20132d04f79
Alex981110 <alex.mtz873@gmail.com>
2022-02-20 21:20:56 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Author from './Components/Author/Author';
 import Post from './Components/Post/Post';
 import Stories from './Components/Stories/Stories';
+import Subes from './Components/subes/Subes';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Stories />}/>
         <Route path="/post" element={<Post />}/>
         <Route path="/author" element={<Author />}/>
+        <Route path="/subes" element={<Subes />}/>
 
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
